8495b9a16dffda2aa19ed4d44b4c5feec0328f78
Navruzbek2103 <navrozbek2306@gmail.com>
2023-01-25 18:36:12 +0500

    Edit search menu

--- js/main.js
+++ js/main.js
@@ -24,13 +24,13 @@ const allCards = document.querySelectorAll(".small-header");
 
 allCards.forEach((element) => {
   let urlLink = window.getComputedStyle(element, ":after").backgroundImage;
-  element.addEventListener("click", (e) => {
+  element.addEventListener("click", () => {
     window.location.replace("allDocs.html");
   });
 });
 
 let lupa = document.querySelector(".lupa");
-let lupa2 = document.querySelector(".lupa-2");
+let lupa2 = document.querySelector(".lupa2");
 const Search = document.querySelector(".search");
 
 lupa.addEventListener("click", () => {
